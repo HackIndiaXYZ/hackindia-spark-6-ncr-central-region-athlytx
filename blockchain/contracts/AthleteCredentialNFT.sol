21ec477012fa316bd8fa9999058932d4dcb1e638
himanshkataria22-maker <himanshkataria22-maker@users.noreply.github.com>
2026-04-05 21:12:28 +0530

    feat(sc): add AthleteCredentialNFT smart contract and hardhat config

--- blockchain/contracts/AthleteCredentialNFT.sol
+++ blockchain/contracts/AthleteCredentialNFT.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract AthleteCredentialNFT is ERC721, ERC721URIStorage, Ownable {
-    uint256 private _tokenIds;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     // Credential types
     enum CredentialType {
@@ -53,8 +55,8 @@ contract AthleteCredentialNFT is ERC721, ERC721URIStorage, Ownable {
         string memory ipfsHash,
         uint256 validityDays
     ) public onlyOwner returns (uint256) {
-        _tokenIds++;
-        uint256 newTokenId = _tokenIds;
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
 
         _safeMint(athleteAddress, newTokenId);
 
